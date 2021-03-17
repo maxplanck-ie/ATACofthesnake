1f8a783e859abd58c5618106f3a6989a0c4f4412
WardDeb <ward@deboutte.be>
2021-03-17 11:59:10 +0100

    remove flake from reqs

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
                       "envs/*yaml"]},
    include_package_data=True,
    install_requires=['configparser', 'rich', 'snakemake', 'pandas',
-                     'seaborn', 'matplotlib', 'numpy', 'flake8'],
+                     'seaborn', 'matplotlib', 'numpy'],
    python_requires='>3'
 )
