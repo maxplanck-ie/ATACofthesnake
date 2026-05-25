6e7d9ab5e5d2bd093d887867c15adbde5698d0c1
WardDeb <ward@deboutte.be>
2026-05-25 06:58:11 +0200

    change help, change alpha default

--- src/aos/atac.py
+++ src/aos/atac.py
@@ -169,10 +169,10 @@ from aos.preflight import Preflight
 @click.option(
     "--gp_alpha",
     required=False,
-    default=0.01,
+    default=1e-10,
     type=float,
     show_default=True,
-    help="the standard deviation for the noise term in the time-course mode. Higher values result in more smoothing (flatter curves). Lower values result in less smoothing (more jagged fits).",
+    help="the variance for the additional noise term in the time-course mode. Higher values result in more smoothing (flatter curves). Lower values result in less smoothing (more jagged fits).",
 )
 def main(**kwargs) -> None:
     pf = Preflight(kwargs)
