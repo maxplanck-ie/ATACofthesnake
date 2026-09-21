f85332d8847a2bd8f28c843c7e9a3a2aa9d6f49c
WardDeb <ward@deboutte.be>
2026-09-21 20:39:58 +0200

    drop unused code, include sanity test for ss/comp, decouple logger

--- src/aos/logger.py
+++ src/aos/logger.py
@@ -3,16 +3,18 @@ from rich.logging import RichHandler
 from pathlib import Path
 
 def setup_logger(logfile: Path) -> logging.Logger:
-    _logger = logging.getLogger()
+    _logger = logging.getLogger("aos")
+    _logger.handlers.clear()
+    _logger.propagate = False
+
     rich_handler = RichHandler(rich_tracebacks=True, show_time=False, show_level=True, show_path=False)
     _fmt = logging.Formatter('%(levelname)s - %(asctime)s - %(message)s', datefmt="%H:%M:%S")
     file_handler = logging.FileHandler(logfile, mode='w')
-    
+
     file_handler.setFormatter(_fmt)
     _logger.setLevel(logging.DEBUG)
     file_handler.setLevel(logging.DEBUG)
     rich_handler.setLevel(logging.DEBUG)
-        
 
     _logger.addHandler(rich_handler)
     _logger.addHandler(file_handler)
